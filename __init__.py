e40a6aa9c5a84b58a78a7d6159f5a8b018bf8105
Hugh Berryman <hgberryman8@gmail.com>
2014-11-25 15:45:31 -0800

    Linked login/landing pages // added sticky footer to header.html

--- __init__.py
+++ __init__.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 def home_page():
 	return render_template('landing.html')
 
-@app.route('/login')
+@app.route('/login/')
 def login_page():
 	return render_template('login.html')
+
+@app.route('/footer')
+def footer_page():
+	return render_template('footer.html')
 
 #practicing git merge - It worked! Boo ya!
 
